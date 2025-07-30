76a9427f612e94df86a55fd999024e5fe0837b9c
Krone <krone.cai@outlook.com>
2025-07-30 10:56:22 +0800

    add pm2 ecoystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    // mqtt后台订阅服务
+    {
+    name: "mqtt-sub-svc",
+    script: "/Users/kronecai/project/mqtt_sub_svc/mqtt-sub-svc/main.py", // 脚本路径
+    interpreter: "/Users/kronecai/project/mqtt_sub_svc/venv/bin/python3",  // 虚拟环境解释器
+    cwd: "/Users/kronecai/project/mqtt_sub_svc",                   // 工作目录
+    autorestart: true,                         // 自动重启
+    }]
+};
